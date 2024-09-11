30533275c7d0c14d574fddccc2249615b1d0f9e9
shuweikejilca <103369808+shuweikejilca@users.noreply.github.com>
2024-09-11 14:24:01 +0800

    Delete car/Two-wheel_balance_car_SL/Hardware directory

--- car/Two-wheel_balance_car_SL/Hardware/inc/encoder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ENCODER_H
-#define __ENCODER_H
-
-#include "stm32f10x.h"                  // Device header
-void Encoder_Init(void);
-int16_t Encoder1_Get(void);
-int16_t Encoder2_Get(void);
-#endif
