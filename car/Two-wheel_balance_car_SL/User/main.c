f9f283807877ae4dc3ec470fdaf5b889ca2a3213
shuweikejilca <103369808+shuweikejilca@users.noreply.github.com>
2024-09-11 14:08:13 +0800

    Add files via upload

--- car/Two-wheel_balance_car_SL/User/main.c
+++ car/Two-wheel_balance_car_SL/User/main.c
@@ -1,10 +1,14 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
+#include "encoder.h"
 
 int main(void){
 	
 	OLED_Init();
+	Encoder_Init();
+	OLED_ShowString(0,0,"Hello",OLED_8X16);
+	OLED_Update();
 	
 	while(1)
 	{
